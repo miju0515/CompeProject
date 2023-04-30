0bccde8a830ce1fd7fbab72709775bfd0f20e98b
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-04-30 17:21:55 +0900

    #3 feat: 정렬기능 추가
    정렬 기능 구현 시도
    프론트 약간 변경

--- todaytodo/src/main/java/project/todaytodo/controller/TodoController.java
+++ todaytodo/src/main/java/project/todaytodo/controller/TodoController.java
@@ -9,6 +9,7 @@ import project.todaytodo.todo.Todo;
 import project.todaytodo.todo.TodoService;
 import project.todaytodo.todo.TodoServiceImpl;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Controller
@@ -33,6 +34,36 @@ public class TodoController {
 //        return "todo/mainPage";
 //    }
 
+
+    @GetMapping("/todo/{sortingway}")
+    public String SortingPage(@RequestParam(required = false,defaultValue = "sortbyId") String sortingway,Model model){
+        List<Todo> sortTodo = todoService.findAllTodo();
+        if(sortingway.equals("sortbyId")){
+            sortTodo.sort(new Comparator<Todo>() {
+                @Override
+                public int compare(Todo o1, Todo o2) {
+                    return Long.compare(o1.getId(),o2.getId()) == 1?-1:Long.compare(o1.getId(),o2.getId())==0?0:1;
+                }
+            });
+        }else if(sortingway.equals("sortbyPre")){
+            sortTodo.sort(new Comparator<Todo>() {
+                @Override
+                public int compare(Todo o1, Todo o2) {
+                    return Integer.compare(o1.getPreference_point(),o2.getPreference_point())==1?-1:Integer.compare(o1.getPreference_point(),o2.getPreference_point());
+                }
+            });
+        }else if(sortingway.equals("sortbyImp")){
+            sortTodo.sort(new Comparator<Todo>() {
+                @Override
+                public int compare(Todo o1, Todo o2) {
+                    return Integer.compare(o1.getImportance_point(),o2.getImportance_point())==1?-1:Integer.compare(o1.getImportance_point(),o2.getImportance_point());
+                }
+            });
+        }
+
+        model.addAttribute("todos",sortTodo);
+        return "todo/mainPage";
+    }
 
     @PostMapping("/todo/new")
     public String create(TodoForm form){
--- todaytodo/src/main/java/project/todaytodo/todo/MemoryTodoRepository.java
+++ todaytodo/src/main/java/project/todaytodo/todo/MemoryTodoRepository.java
@@ -29,3 +29,4 @@ public class MemoryTodoRepository implements TodoRepository {
         store.clear();
     }
 }
+
